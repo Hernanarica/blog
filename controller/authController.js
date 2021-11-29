7369dbac60b4a5e091b1e2cf1967df6ea46d74b5
Hernán Arica <hernanaricammm@gmail.com>
2021-11-28 21:47:14 -0300

    Agregado el token en el json de respuesta al login

--- controller/authController.js
+++ controller/authController.js
@@ -4,7 +4,7 @@ import { createToken } from "../middleware/auth.js";
 export function login(req, res) {
 	auth.login(req.body.email, req.body.password).then(r => {
 		const token = createToken(r);
-		res.json(r);
+		res.json({ r, token });
 	}).catch(err => {
 		res.status(400).json({ msg: err });
 	});
