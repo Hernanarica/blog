15ab811bf455afbd319184b2f3caf1585d7fb8f2
Hernán <hernanaricammm@gmail.com>
2021-12-02 21:38:34 -0300

    ABM de opiniones listo

--- model/opinions.js
+++ model/opinions.js
@@ -1,9 +1,9 @@
 import DBConnection from "./database.js";
 import { ObjectId } from "mongodb";
 
-export async function create(comment) {
+export async function create(opinion) {
 	return DBConnection(async db => {
-		return await db.collection('opinions').insertOne(comment);
+		return await db.collection('opinions').insertOne(opinion);
 	});
 }
 
@@ -38,5 +38,38 @@ export async function getAllPublished() {
 	return DBConnection(async db => {
 		const opinions = await db.collection('opinions').find().toArray();
 		return opinions.filter(opinion => opinion.public === true);
+	});
+}
+
+export async function getById(id) {
+	return DBConnection(async db => {
+		return await db.collection('opinions').findOne({ _id: ObjectId(id) });
+	});
+}
+
+export async function edit(opinionData) {
+	const opinion = { ...opinionData };
+
+	return DBConnection(async db => {
+		const oldOpinion = await db.collection('opinions').findOne({ _id: ObjectId(opinion.id) });
+		opinion.public   = oldOpinion.public;
+		opinion.created  = oldOpinion.created;
+
+		if (oldOpinion) {
+			return await db.collection('opinions').replaceOne(
+				 {
+					 _id: ObjectId(opinion.id)
+				 },
+				 {
+					 name: opinion.name,
+					 lastname: opinion.lastname,
+					 text: opinion.text,
+					 created: opinion.created,
+					 public: opinion.public
+				 }
+			);
+		}
+
+		throw ({ msg: 'La opinión no fue encontrada' });
 	});
 }
