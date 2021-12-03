c2e4c5e0af77889cefcb0a942bc84eb0fb0c3a0f
beto <56642267+be-tho@users.noreply.github.com>
2021-12-03 09:04:34 -0300

    fix PostController

--- model/post.js
+++ model/post.js
@@ -1,18 +1,20 @@
+export default Panel;
+
 import DBConnection from "./database.js";
 import { ObjectId } from "mongodb";
 
 export async function create(postData) {
-	return DBConnection(async db => {
-		const newPost = { ...postData };
-
-		return await db.collection('posts').insertOne(newPost);
-	});
+   return DBConnection(async db => {
+      const newPost = { ...postData };
+      
+      return await db.collection('posts').insertOne(newPost);
+   });
 }
 
 export async function getAll() {
-	return DBConnection(async db => {
-		return await db.collection('posts').find().toArray();
-	});
+   return DBConnection(async db => {
+      return await db.collection('posts').find().toArray();
+   });
 }
 
 export async function getById(id) {
@@ -22,55 +24,55 @@ export async function getById(id) {
 }
 
 export async function getAllPublished() {
-	return DBConnection(async db => {
-		const posts = await db.collection('posts').find().toArray();
-		return posts.filter(post => post.public === true);
-	});
+   return DBConnection(async db => {
+      const posts = await db.collection('posts').find().toArray();
+      return posts.filter(post => post.public === true);
+   });
 }
 
 export async function remove(id) {
-	return DBConnection(async db => {
-		return await db.collection('posts').deleteOne({ _id: ObjectId(id) });
-	});
+   return DBConnection(async db => {
+      return await db.collection('posts').deleteOne({ _id: ObjectId(id) });
+   });
 }
 
 export async function published(id) {
-	return DBConnection(async db => {
-		return await db.collection('posts').updateOne(
-			 {
-				 _id: ObjectId(id)
-			 },
-			 {
-				 $set: {
-					 public: true
-				 }
-			 }
-		);
-	});
+   return DBConnection(async db => {
+      return await db.collection('posts').updateOne(
+          {
+             _id: ObjectId(id)
+          },
+          {
+             $set: {
+                public: true
+             }
+          }
+      );
+   });
 }
 
 export async function edit(postData) {
-	const post = { ...postData };
-
-	return DBConnection(async db => {
-		const oldPost = await db.collection('posts').findOne({ _id: ObjectId(post.id) });
-		post.public   = oldPost.public;
-		post.created  = oldPost.created;
-
-		if (oldPost) {
-			return await db.collection('posts').replaceOne(
-				 {
-					 _id: ObjectId(post.id)
-				 },
-				 {
-					 title: post.title,
-					 text: post.text,
-					 created: post.created,
-					 public: post.public
-				 }
-			);
-		}
-
-		throw ({ msg: 'El post no fue encontrado' });
-	});
+   const post = { ...postData };
+   
+   return DBConnection(async db => {
+      const oldPost = await db.collection('posts').findOne({ _id: ObjectId(post.id) });
+      post.public   = oldPost.public;
+      post.created  = oldPost.created;
+      
+      if (oldPost) {
+         return await db.collection('posts').replaceOne(
+             {
+                _id: ObjectId(post.id)
+             },
+             {
+                title: post.title,
+                text: post.text,
+                created: post.created,
+                public: post.public
+             }
+         );
+      }
+      
+      throw ({ msg: 'El post no fue encontrado' });
+   });
 }
